3bc0e3c92bc4a7ad4bcf9c0bbaa9eecde38315b7
Daddy91 <daddymosanda@live.fr>
2023-12-14 15:08:33 +0100

    Changed the Hello2

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello Daddy, Welcome to Devember!</Text>
+      <Text>Hello Daddy, Welcome to Devember!!</Text>
       <StatusBar style="auto" />
     </View>
   );
