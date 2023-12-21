2196e999115408b1972dd5422673fca27ab62e0c
Daddy91 <daddymosanda@live.fr>
2023-12-21 17:16:52 +0100

    Load fonts in the Root Layout

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -7,33 +7,10 @@ import {
   ActivityIndicator,
 } from "react-native";
 import DayListItem from "../../src/components/core/DayListItem";
-import { useEffect } from "react";
-
-import { useFonts, Inter_900Black } from "@expo-google-fonts/inter";
-import {AmaticSC_400Regular, AmaticSC_700Bold} from '@expo-google-fonts/amatic-sc';
-import * as SplashScreen from "expo-splash-screen";
-
-SplashScreen.preventAutoHideAsync();
 
 const days = [...Array(24)].map((val, index) => index + 1);
 
-export default function HomeScreen() {
-  const [fontsLoaded, fontError] = useFonts({
-    Inter: Inter_900Black,
-    Amatic: AmaticSC_400Regular,
-    AmaticBold: AmaticSC_700Bold,
-  });
-
-  useEffect(() => {
-    if (fontsLoaded || fontError) {
-      SplashScreen.hideAsync();
-    }
-  }, [fontsLoaded, fontError]);
-
-  if (!fontsLoaded && !fontError) {
-    return null;
-  }
-
+export default function HomeScreen() {  
   return (
     <View style={styles.container}>
       <FlatList
